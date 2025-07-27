a2d2f03bbf454fb2582f166b56da526d79d7504a
Joshua Gomez <joshuago78@gmail.com>
2025-07-27 12:02:48 -0700

    completed 2015 day03

---
+++ 2015/day03.py
@@ -0,0 +1,50 @@
+
+
+MOVES = {
+    '^': ( 0, 1),
+    'v': ( 0,-1),
+    '>': ( 1, 0),
+    '<': (-1, 0)
+}
+
+
+def parse(raw_data):
+    return [MOVES[char] for char in raw_data.strip()]
+
+
+def part1(raw_data):
+    moves = parse(raw_data)
+    x,y = 0,0
+    houses = {x:[y,]}
+    for move in moves:
+        x,y = tuple(map(sum, zip((x,y), move)))
+        if x in houses.keys():
+            if y not in houses[x]:
+                houses[x].append(y)
+        else:
+            houses[x] = [y,]
+    return sum([len(h) for h in houses.values()])
+
+
+def part2(raw_data):
+    moves = parse(raw_data)
+    santa = [m for i,m in enumerate(moves) if i%2==0]
+    robot = [m for i,m in enumerate(moves) if i%2!=0]
+    x,y = 0,0
+    houses = {x:[y,]}
+    for move in santa:
+        x,y = tuple(map(sum, zip((x,y), move)))
+        if x in houses.keys():
+            if y not in houses[x]:
+                houses[x].append(y)
+        else:
+            houses[x] = [y,]
+    x,y = 0,0
+    for move in robot:
+        x,y = tuple(map(sum, zip((x,y), move)))
+        if x in houses.keys():
+            if y not in houses[x]:
+                houses[x].append(y)
+        else:
+            houses[x] = [y,]
+    return sum([len(h) for h in houses.values()])
